71eec0925f913f06b22afa516a94f0f4e4c2080b
Mona-Mokha <mmokhala@my.centennialcollege.ca>
2025-09-26 22:13:58 -0400

    Final_version

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,17 +2,16 @@ export default function Home() {
     return (
         <div>
             <h2>Hello! I am Mona Mokhalalati</h2>
-            <p>
+            <p className="welcome-text">
                 Welcome to my portfolio
-
             </p>
 
-            <p className="mt-4">
+
+
+            <p>
                 I am excited to share my work with you. Please take a look at my projects and feel free to reach out if you have any questions or would like to collaborate.
             </p>
-            <div className="mt-6">
-                <img src="/logo.png" alt="Mona Mokhalalati" className="w-48 h-48 rounded-full mx-auto" />
-            </div>
+            
         </div>
     );
 }
